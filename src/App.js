8e5b70149ad61b69c83ba5951ed2d6b348b7ad19
yujinkim11 <wowyj26@naver.com>
2022-07-26 18:49:05 +0900

    컴포너트 합치고 props 오류 수정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import { Box } from "./Box";
 import "./App.css";
-import { Button } from "./Button";
+import { useState } from "react";
+
+const choice = {
+  rock: {
+    name: "Rock",
+    img: "https://nationaltoday.com/wp-content/uploads/2021/08/National-Pet-Rock-Day-300x300.jpg",
+  },
+  scissor: {
+    name: "Scissor",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDJJO2TmRnaDnTqYWMx4J0FGXw2aEKl4m9ow&usqp=CAU",
+  },
+  paper: {
+    name: "Paper",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSU1lmPmCyOvRs1bhjrTNQ4KZ7XRZxGllgRCQ&usqp=CAU",
+  },
+};
 
 function App() {
+  const [select, setSelect] = useState(null);
+
+  const play = (userChoice) => {
+    setSelect(choice[userChoice]);
+  };
+
   return (
     <>
       <div className="mainbox">
@@ -10,7 +31,9 @@ function App() {
         <Box user="Computer" />
       </div>
       <div className="mainbox">
-        <Button />
+        <button onClick={() => play("scissors")}>가위</button>
+        <button onClick={() => play("rock")}>바위</button>
+        <button onClick={() => play("paper")}>보</button>
       </div>
     </>
   );
--- src/Box.js
+++ src/Box.js
@@ -9,12 +9,12 @@ const BoxWrap = styled.div`
   } */
 `;
 
-export const Box = ({ user }) => {
+export const Box = (props) => {
   console.log("props", props);
   return (
     <BoxWrap>
       <div>
-        <h1>{user}</h1>
+        <h1>user</h1>
         <img src="https://media.istockphoto.com/photos/stone-pebble-gray-picture-id1288973456?b=1&k=20&m=1288973456&s=170667a&w=0&h=GBGgp4yrZv4ooDBws8yHF24sJ3rkEpObYsBWpVNKFT8=" />
         <h2>결과값</h2>
       </div>
