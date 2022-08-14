9cb53eb0affa8dbab50a96cf27e7b818ef486664
yujinkim11 <wowyj26@naver.com>
2022-08-14 17:55:41 +0900

    deploy

--- src/Box.js
+++ src/Box.js
@@ -30,14 +30,11 @@ export const Box = (props) => {
     props.result !== "tie" &&
     props.result !== ""
   ) {
-    // 카드가 computer카드인가? && 결과가 비긴건 아닌가? && props.result에 값이 있는가?
     result = props.result === "win" ? "lose" : "win";
   } else {
-    // 위의 경우가 아니라면 props 로 전달된 결과를 그대로 쓴다.
     result = props.result;
   }
   if (props.title === "Computer") {
-    // console.log("computer", result);
   }
 
   return (
